486f06fc84dbca4aa7a9bd0ba44f6b78c39f5c5b
Matsuyama Tomomasa <rainmakermx5269@gmail.com>
2018-07-05 14:46:04 +0900

    Third Commit on thw Eclipse！

--- HelloWorld/src/sample/HelloWorld.java
+++ HelloWorld/src/sample/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
         System.out.println("こんにちは世界！");
-        System.out.println("Hasta manana！");
+
     }
 
 }
